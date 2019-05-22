31f9ee032aee8442940add8248bf978c17db0961
Paul Irofti <paul@irofti.net>
2019-05-22 10:11:41 +0000

    Check for pip and install

--- specs/default/cluster-init/scripts/10.install_pypkgs.sh
+++ specs/default/cluster-init/scripts/10.install_pypkgs.sh
@@ -8,5 +8,8 @@ PY_PACKAGES=dask
 # Azure VMs that have ephemeral storage have that mounted at /mnt/resource. If that does not exist this command will create it.
 mkdir -p /mnt/resource/apps
 
+# Check for pip and install it
+which pip > /dev/null 2>&1 || yum -y install python-pip
+
 # Install python packages
 pip install ${PY_PACKAGES}
